865cd1988db62f91002c1dc52753cf5f364876b4
Alexandra Wells <aw771620@ohio.edu>
2022-01-18 19:43:11 -0500

    Test program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alexandra Wells
+ *        Date: 1/13/2022
+ * Description: Prints one string
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
